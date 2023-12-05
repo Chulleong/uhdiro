6848118214bb3e0d412574e11bedf9ba3da3a15e
Chulleong <tuna1239@kakao.com>
2023-12-06 03:15:17 +0900

    add: main.page

--- main.js
+++
@@ -1,8 +0,0 @@
-//페이지 로드 이벤트
-window.addEventListener("load", ()=>{
-  const grid = new Isotope("section", {
-    itemSelector: "article",
-    columnWidth: "article",
-    transitionDuration: "0.5s"
-  })
-})

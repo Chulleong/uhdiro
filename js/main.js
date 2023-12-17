116edfddc05a94f5970d90fb1cb31947ac2f1c9d
Chulleong <tuna1239@kakao.com>
2023-12-17 23:58:36 +0900

    1217

--- js/main.js
+++ js/main.js
@@ -4,47 +4,135 @@
     level: 6
   };
 
+  let kakaoMapDiv = document.querySelector("#kakao-map")
+
   var map = new kakao.maps.Map(container, options);
 
+  const middleDiv = document.querySelectorAll(".middle-div > article > div")
 
-  var positions = [
-    {
-        title: '충남대', 
-        latlng: new kakao.maps.LatLng(36.3688253, 127.3468684)
-    },
-    {
-        title: '한화이글스', 
-        latlng: new kakao.maps.LatLng(36.3172026, 127.4285703)
-    },
-    {
-        title: '텃밭', 
-        latlng: new kakao.maps.LatLng(33.450879, 126.569940)
-    },
-    {
-        title: '근린공원',
-        latlng: new kakao.maps.LatLng(33.451393, 126.570738)
-    }
-];
+  console.log(middleDiv)
 
-var linePath = [
-  new kakao.maps.LatLng(36.3688253, 127.3468684),
-  new kakao.maps.LatLng(36.3172026, 127.4285703),
-];
+  const deajeon = [{
+    title: "충남대",
+    x: "36.3688253",
+    y: "127.3468684",
+    startx: "36.3504119",
+    starty: "127.3845475",
+    level: 9
+  },{
+    title: "한화이글스",
+    x: "36.3172026",
+    y: "127.4285703"
+  },{
+    title: "성심당",
+    x: "36.3323220",
+    y: "127.4000000"
+  }]
 
-var polyline = new kakao.maps.Polyline({
-  path: linePath, // 선을 구성하는 좌표배열 입니다
-  strokeWeight: 5, // 선의 두께 입니다
-  strokeColor: '#FFAE00', // 선의 색깔입니다
-  strokeOpacity: 0.7, // 선의 불투명도 입니다 1에서 0 사이의 값이며 0에 가까울수록 투명합니다
-  strokeStyle: 'solid' // 선의 스타일입니다
-});
+  const deajeon1 = [{
+    title: "충남대",
+    x: "36.7688253",
+    y: "127.3468684",
+    startx: "36.7504119",
+    starty: "127.345475",
+    level: 10
+  },{
+    title: "한화이글스",
+    x: "36.7172026",
+    y: "127.4285703"
+  },{
+    title: "성심당",
+    x: "36.7323220",
+    y: "127.4000000"
+  }]
 
-polyline.setMap(map);
+  middleDiv.forEach(element => {
+    element.addEventListener("click", () => {
+      positions = []
+      linePath = []
+      options = {}
+      if(element.id == "1"){
+        for(let i = 0; i<deajeon.length; i++){
+          positions.push({
+            title: deajeon[i].title,
+            latlng: new kakao.maps.LatLng(deajeon[i].x, deajeon[i].y)
+          })
+          linePath.push(
+            new kakao.maps.LatLng(deajeon[i].x, deajeon[i].y)
+          )
+        }
+        options.center = new kakao.maps.LatLng(deajeon[0].startx, deajeon[0].starty)
+        options.level = deajeon[0].level
+      }else if(element.id == "2"){
+        for(let i = 0; i<deajeon1.length; i++){
+          positions.push({
+            title: deajeon1[i].title,
+            latlng: new kakao.maps.LatLng(deajeon1[i].x, deajeon1[i].y)
+          })
+          linePath.push(
+            new kakao.maps.LatLng(deajeon1[i].x, deajeon1[i].y)
+          )
+        }
+        options.center = new kakao.maps.LatLng(deajeon1[0].startx, deajeon1[0].starty)
+        options.level = deajeon1[0].level
+      }
+
+      draw()
+    })
+  });
+
+  let positions = []
+
+  let linePath = []
+
+
+
+
+//   var positions = [
+//     {
+//         title: '충남대', 
+//         latlng: new kakao.maps.LatLng(36.3688253, 127.3468684)
+//     },
+//     {
+//         title: '한화이글스', 
+//         latlng: new kakao.maps.LatLng(36.3172026, 127.4285703)
+//     },
+//     {
+//         title: '텃밭', 
+//         latlng: new kakao.maps.LatLng(33.450879, 126.569940)
+//     },
+//     {
+//         title: '근린공원',
+//         latlng: new kakao.maps.LatLng(33.451393, 126.570738)
+//     }
+// ];
+
+// var linePath = [
+//   new kakao.maps.LatLng(36.3688253, 127.3468684),
+//   new kakao.maps.LatLng(36.3172026, 127.4285703),
+// ];
+
+
 
 // 마커 이미지의 이미지 주소입니다
 var imageSrc = "https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png"; 
-    
-for (var i = 0; i < positions.length; i ++) {
+
+const draw = () => {
+  kakaoMapDiv.innerHTML = ""
+  const div = document.createElement("div")
+  div.id = "map"
+  div.style = "width:480px;height:700px;"
+  kakaoMapDiv.appendChild(div)
+  var map = new kakao.maps.Map(div, options);
+  var polyline = new kakao.maps.Polyline({
+    path: linePath, // 선을 구성하는 좌표배열 입니다
+    strokeWeight: 5, // 선의 두께 입니다
+    strokeColor: '#FFAE00', // 선의 색깔입니다
+    strokeOpacity: 0.7, // 선의 불투명도 입니다 1에서 0 사이의 값이며 0에 가까울수록 투명합니다
+    strokeStyle: 'solid' // 선의 스타일입니다
+  });
+
+  for (var i = 0; i < positions.length; i ++) {
     
     // 마커 이미지의 이미지 크기 입니다
     var imageSize = new kakao.maps.Size(24, 35); 
@@ -59,4 +147,7 @@ for (var i = 0; i < positions.length; i ++) {
         title : positions[i].title, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
         image : markerImage // 마커 이미지 
     });
+  }
+
+  polyline.setMap(map);
 }
